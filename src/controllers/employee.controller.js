4e50a33c420b95ad8d3942e864061f4f79a82146
ankush1423 <139746905+ankush1423@users.noreply.github.com>
2024-12-31 10:06:39 +0530

    add functionality to employee controller

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -1,6 +1,5 @@
 import { mySqlPool } from "../db/database.js";
 
-
 export const getAllEmployees = async (req,res) => {
     try
     {
@@ -13,3 +12,51 @@ export const getAllEmployees = async (req,res) => {
     }
 }
 
+export const getEmployeeById = async(req,res) =>{
+      try 
+      { 
+        const {employeeId} = req.params
+
+        if(!employeeId)
+        {
+           throw new Error("error while getting the id")
+        }
+  
+        const [record] = await mySqlPool.query(`SELECT * FROM employees WHERE id = ?`,[employeeId])
+
+        if(record.length == 0)
+        {
+            return res.send("there is no employee with this id")
+        }
+        return res.json(record)
+      } 
+      catch (error)
+      {
+         console.log("ERROR WHILE GETTING THE EMPLOYEE DATA ",error)
+      }
+}
+
+export const deleteEmployeeData = async(req,res) => {
+     try
+     {
+        const {employeeId} = req.params
+
+        if(!employeeId)
+        {
+            throw new Error("ERROR WHILE GETTING THE EMPLOYEE ID")
+        }
+
+        const [record] = await mySqlPool.query(`DELETE FROM employees WHERE id = ?`,[employeeId])
+        
+        if(record.affectedRows == 0)
+        {
+            return res.send("there is no employee with this id")
+        }
+        
+        return res.send("employee deleted SuccessFully")
+     }
+     catch(err)
+     {
+        console.log("ERROR WHILE DELETING THE EMPLOYEE DATA : ",err)
+     }
+}
--- src/routes/employees.route.js
+++ src/routes/employees.route.js
@@ -1,5 +1,10 @@
 import express from "express"
-import { getAllEmployees } from "../controllers/employee.controller.js";
+import { 
+       getAllEmployees,
+       getEmployeeById, 
+       deleteEmployeeData,
+
+       } from "../controllers/employee.controller.js";
 
 
 const router = express.Router()
@@ -7,5 +12,6 @@ const router = express.Router()
 
 router.get("/",getAllEmployees)
 
+router.route("/:employeeId").get(getEmployeeById).delete(deleteEmployeeData)
 
 export default router;
